d10d1458c1265a8140fb82b41fc76b2c30dcbb71
Pozhidaev Nikita <Pozidaevnikita85@gmail.com>
2024-09-25 12:38:03 +0300

    Добавил пару сценариев Списков покупок

--- Playwright_lessons/tests/Lesson_1.test.ts
+++ Playwright_lessons/tests/Lesson_1.test.ts
@@ -49,3 +49,51 @@ test('(АЗ, ФЛ) Корзина. Действия в корзине - удал
 // Корзина очищена
   await page.getByText('Корзина пуста').isVisible();
 });
+
+// 
+export const cookies = [
+    { name: 'experimental', value: '1', domain: 'stagespa.dev.baucenter.ru', path: '/' },
+    { name: 'authorization', value: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIwNWFjN2E1Mi03YjFhLTExZWYtYmE5Yi02Mjg1ODgzZTJhZjEiLCJsb2MiOjExMDAwMDAwMzcsImV4cCI6MTcyOTg0NTc2NCwicm9sZSI6Im5vLWNhcmQiLCJzdWIiOjIwNTAyMDEsInRlbCI6Ijc5MjAyNDgxNDQwIn0._09KvdzTqBcT4siYSunfK5cvPkZuUEhxo4I6PWpuRIE', domain: 'stagespa.dev.baucenter.ru', path: '/' }
+  ];
+// TODO Добавить моки
+test('Списки покупок - создать список покупок (нет списков)', async ({ page, context }) => {
+    test.setTimeout(36000);
+    await context.addCookies(cookies);
+// Предусловие: нет созданных списков покупок
+
+// Открыть страницу "Списки покупок"
+    await page.goto('https://stagespa.dev.baucenter.ru/personal/list/');
+// Страница открыта
+    await page.getByText('Списки покупок').nth(1).isVisible();
+// Тап на "Создать список"
+    await page.getByRole('button', {name: 'Создать список', exact: true}).click();
+// Открыто окно "Создание списка"
+    await page.getByText('Создание списка').isVisible();
+// Поле "Название списка покупок" предзаполнено значением "Новый список"
+    await page.getByText('Новый список').isVisible();
+// Поле "Комментарий" пустое
+
+// Тап на "Создать список" в открытом окне
+    await page.getByRole('button', {name: 'Создать список', exact: true}).nth(1).click();
+// Список создан, отображается снэк об успешном создании списка покупок
+    await page.getByText('Создан новый список покупок «Новый список»').isVisible();
+});
+
+// TODO Добавить моки и разнести все по файлам
+test('Списки покупок. Список Х - удалить список ', async ({ page, context }) => {
+  test.setTimeout(36000);
+  await context.addCookies(cookies);
+  await page.goto('https://stagespa.dev.baucenter.ru/personal/list/');
+  // Перейти в деталку списка покупок
+  await page.locator('.styled__InfoContainer-sc-1dq25wk-4.cdFZck').first().click();
+  // Открылся список покупок
+
+  // Клик иконку удаления списка 
+  await page.locator('section').filter({ hasText: 'Новый списокСписок покупок пустДобавляйте в этот список товары из каталога или к' }).getByRole('button').nth(1).click();
+  // Открылось окно "Удалить список покупок?" 
+  await page.getByText('Вы действительно хотите удалить этот список покупок?').isVisible();
+  // Клик на "Удалить"
+  await page.getByRole('button', {name: 'Удалить', exact: true}).click();
+  // Список удален, снек "Список удален". Открылся экран с перечнем списков
+  await page.getByText('Список покупок удалён').isVisible();
+});
